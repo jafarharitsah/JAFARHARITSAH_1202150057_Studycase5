ddf7fc8e9fd15547004e3feac104581426eb9fe9
Jafar Haritsah <jafar628@gmail.com>
2018-03-25 18:38:37 +0700

    push 18.39 25-03-2018

---
+++ JAFARHARITSAH_1202150057_Studycase5/app/src/main/java/com/example/android/JAFARHARITSAH_1202150057_Studycase5/MyDatabase.java
@@ -0,0 +1,17 @@
+package com.example.android.JAFARHARITSAH_1202150057_Studycase5;
+
+import android.provider.BaseColumns;
+
+    //class yang digunakan untuk mendefinisikan skema database
+public class MyDatabase {
+
+    public static final class DatabaseScheme implements BaseColumns {
+        public static final String TABLE_NAME = "table_name";
+        public static final String DATABASE_ID = "ID";
+        public static final String NAMA_TODO ="todoname";
+        public static final String DESKRIPSI = "description";
+        public static final String PRIORITAS = "priority";
+    }
+
+
+}
